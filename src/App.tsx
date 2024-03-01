53605b1d67d93e90c2606e5d4e7ac177d46f607a
amit.raut <amitraut11@gmail.com>
2024-03-01 16:35:17 -0600

    added react files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calc from './Calc';
 
 function App() {
   return (
     <div className="App">
-   
+    <Calc/>
     </div>
   );
 }
--- src/Calc.tsx
+++ src/Calc.tsx
@@ -1,10 +1,84 @@
+import { setDefaultResultOrder } from "dns";
 import React,{useState} from "react";
 
 function Calc() {
+    const [num1,setNum1] = useState(0);
+    const [num2,setNum2] = useState(0);
+    const [operator,setOperator] = useState('+');
+    const [result,setResult] = useState(0);
+
+    const handleNum1Change = (event : React.ChangeEvent<HTMLInputElement>) => {
+
+        setNum1(parseFloat(event.target.value))
+    }
+
+    const handleNum2Change = (event : React.ChangeEvent<HTMLInputElement>) => {
+
+        setNum2(parseFloat(event.target.value))
+    }
+
+    const handleOperatorChange = (event : React.ChangeEvent<HTMLSelectElement>)=>
+    {
+        setOperator (event.target.value)
+    }
+
+    const handleCalculate = () => {
+
+        let calcResult = 0;
+
+        switch (operator)
+        {
+            case '+' :
+                calcResult = num1+num2 ;
+                break ;
+
+            case '-' :
+                calcResult = num1-num2 ;
+                break ;
+
+            case '*' :
+                calcResult = num1*num2 ;
+                break ;
+
+            case '/' :
+                calcResult = num1/num2 ;
+                break ;
+
+        }
+        
+        setResult(calcResult);
+    }
+    
     return (
-        <div>
-            
+        <div >
+           <label>
+            Enter First Number :
+            <input type="number" value={num1} onChange={handleNum1Change} />
+          </label>
+          <br />
+
+          <label>
+            Enter Second Number :
+            <input type="number" value={num2} onChange={handleNum2Change} />
+          </label>
+          <br />
+
+          <label>
+            Select Operations :
+            <select value={operator} onChange= {handleOperatorChange}>
+            <option value = "+" >+</option>
+            <option value = "-" >-</option>
+            <option value = "*" >*</option>
+            <option value = "/" >/</option>
+            </select>
+          </label>
+          <br />
+          <button onClick={handleCalculate}>Calculate</button> 
+            <br/>
+            final result : {result}
+
         </div>
+       
     )
 }
 
